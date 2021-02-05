50fd7294034d8ed82b6b2337a98312846eeda45a
Cosmos Appiah <cosmosappiah029@gmail.com>
2021-02-05 22:01:29 +0000

    added express json parser

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
-import express, { Application } from "express";
+import express, { Application, json } from "express";
 import { apiErrorHandler } from "./utils/api-error";
 import { AppRouter } from "./utils/app-router";
 import "./controllers/root.controller";
 
 const app: Application = express();
 
+app.use(json());
 app.use(AppRouter.instance);
 app.use(apiErrorHandler);
 
