2931e9c19cb8d9e7aaeea4e3d922ee50329148d1
Cosmos Appiah <cosmosappiah029@gmail.com>
2021-02-05 21:19:14 +0000

    imported the root controller

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { Application } from "express";
 import { apiErrorHandler } from "./utils/api-error";
 import { AppRouter } from "./utils/app-router";
+import "./controllers/root.controller";
 
 const app: Application = express();
 
