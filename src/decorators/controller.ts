d6f6944eca7fc5463d402fc13d14037f0bdacca7
Cosmos Appiah <cosmosappiah029@gmail.com>
2021-02-05 15:01:33 +0000

    added controller decorator

---
+++ src/decorators/controller.ts
@@ -0,0 +1,30 @@
+import "reflect-metadata";
+import { MetadataKeys, Methods } from "src/constants/constant";
+import { AppRouter } from "src/utils/app-router";
+
+const getMetadata = (
+  metadataKey: MetadataKeys,
+  target: Object,
+  propertyKey: string
+): any => {
+  return Reflect.getMetadata(metadataKey, target, propertyKey);
+};
+
+export function controller(prefix: string) {
+  return function (constructor: Function) {
+    const router = AppRouter.instance;
+    for (let key in constructor.prototype) {
+      const handler = constructor.prototype[key];
+      const path = getMetadata(MetadataKeys.PATH, constructor.prototype, key);
+      const method: Methods = getMetadata(
+        MetadataKeys.METHOD,
+        constructor.prototype,
+        key
+      );
+      const middlewares =
+        getMetadata(MetadataKeys.MIDDLEWARE, constructor.prototype, key) || [];
+
+      if (path) router[method](`${prefix}${path}`, ...middlewares, handler);
+    }
+  };
+}
