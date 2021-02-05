118731317ac0b4cf83a1307aa871e35367d9d3f4
Cosmos Appiah <cosmosappiah029@gmail.com>
2021-02-05 14:22:38 +0000

    added route decorator

---
+++ src/decorators/routes.ts
@@ -0,0 +1,22 @@
+import "reflect-metadata";
+import { RequestHandler } from "express";
+import { MetadataKeys, Methods } from "src/constants/constant";
+
+interface RouteHandlerDescriptor extends PropertyDescriptor {
+  value?: RequestHandler;
+}
+
+const routeBinder = (method: string) => {
+  return function (path: string) {
+    return function (target: any, key: string, desc: RouteHandlerDescriptor) {
+      Reflect.defineMetadata(MetadataKeys.METHOD, method, target, key);
+      Reflect.defineMetadata(MetadataKeys.PATH, path, target, key);
+    };
+  };
+};
+
+export const get = routeBinder(Methods.GET);
+export const patch = routeBinder(Methods.PATCH);
+export const post = routeBinder(Methods.POST);
+export const del = routeBinder(Methods.DELETE);
+export const put = routeBinder(Methods.PUT);
