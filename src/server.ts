4888b392de68e939a01bbd635a8b5dacac5b890c
Cosmos Appiah <cosmosappiah029@gmail.com>
2021-02-09 00:36:16 +0000

    fixed imports

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
 import app from "./app";
-import keys from "./constants/config/keys";
+import keys from "./constants/keys";
 
 app.listen(keys.PORT, () => console.log(`server is listening on ${keys.PORT}`));
