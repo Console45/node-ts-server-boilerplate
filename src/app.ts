075eb20f41a40982ea0b8cfc172559e188f68428
Cosmos Appiah <cosmosappiah029@gmail.com>
2021-02-10 01:31:07 +0000

    added connect database function

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
+import { connectDatabase } from "./database/database-config";
 import express, { Application, json } from "express";
 import { apiErrorHandler } from "./utils/api-error";
 import { AppRouter } from "./utils/app-router";
 import "./controllers/root.controller";
 
 const app: Application = express();
+
+//connect database
+connectDatabase();
 
 app.use(json());
 app.use(AppRouter.instance);
