6aeedbbfc363cd3d2759d0cffbbfd21ff18f4f54
Cosmos Appiah <cosmosappiah029@gmail.com>
2021-02-13 23:32:46 +0000

    update

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,13 @@ import { connectDatabase } from "./database/database-config";
 import express, { Application, json } from "express";
 import { apiErrorHandler } from "./utils/api-error";
 import { AppRouter } from "./utils/app-router";
+
 import "./controllers/root.controller";
+import "./controllers/auth.conroller";
 
 const app: Application = express();
 
-//connect database
+// connect database
 connectDatabase();
 
 app.use(json());
