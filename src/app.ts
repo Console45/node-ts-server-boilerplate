3aa7714b121be19a84061eacb717fa555f3bec9f
Cosmos Appiah <cosmosappiah029@gmail.com>
2021-02-16 04:36:42 +0000

    update

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,21 @@
+import "reflect-metadata";
 import { connectDatabase } from "./database/database-config";
 import express, { Application, json } from "express";
+import compression from "compression";
 import { apiErrorHandler } from "./utils/api-error";
 import { AppRouter } from "./utils/app-router";
-
+import cors from "cors";
 import "./controllers/root.controller";
 import "./controllers/auth.conroller";
+import { shouldCompress } from "./utils/app-utils";
 
 const app: Application = express();
 
 // connect database
 connectDatabase();
 
+app.use(cors());
+app.use(compression({ filter: shouldCompress }));
 app.use(json());
 app.use(AppRouter.instance);
 app.use(apiErrorHandler);
