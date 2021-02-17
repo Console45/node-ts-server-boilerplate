97318fc7bde158beb29448a307a6c2d9dc07c688
Cosmos Appiah <cosmosappiah029@gmail.com>
2021-02-17 03:23:49 +0000

    added login path

--- src/controllers/auth.conroller.ts
+++ src/controllers/auth.conroller.ts
@@ -1,3 +1,4 @@
+import { loginParamsSchema, loginSchema } from "./../validators/schema/auth";
 import { ValidationFields } from "./../constants/constant";
 import { Request, Response, NextFunction } from "express";
 import { controller, post, validate } from "../decorators";
@@ -17,6 +18,35 @@ class AuthController {
       res.status(201).json({
         status: "success",
         message: `${user.role} registration is successful.`,
+        data: {
+          user,
+          accessToken,
+        },
+      });
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  @post("/login/:role")
+  @validate(
+    { schema: loginParamsSchema, field: ValidationFields.PARAMS },
+    { schema: loginSchema, field: ValidationFields.BODY }
+  )
+  async postLogin(
+    { body, params }: Request,
+    res: Response,
+    next: NextFunction
+  ) {
+    authServiceInstance.res = res;
+    try {
+      const { user, accessToken } = await authServiceInstance.loginUser(
+        body,
+        params
+      );
+      res.json({
+        status: "success",
+        message: `${user.role} login is successful.`,
         data: {
           user,
           accessToken,
