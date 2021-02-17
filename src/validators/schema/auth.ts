224fe03d3624f43733c0f0378ccb83bbc5d5c6b4
Cosmos Appiah <cosmosappiah029@gmail.com>
2021-02-17 03:22:36 +0000

    added login schemas

--- src/validators/schema/auth.ts
+++ src/validators/schema/auth.ts
@@ -6,3 +6,14 @@ export const registerSchema: ObjectSchema = Joi.object().keys({
   password: Joi.string().min(6).required(),
   role: Joi.string().valid("User", "Admin", "Super Admin").required(),
 });
+
+export const loginSchema: ObjectSchema = Joi.object().keys({
+  email: Joi.string().email().required(),
+  password: Joi.string().min(6).required(),
+});
+
+export const loginParamsSchema: ObjectSchema = Joi.object().keys({
+  role: Joi.string().valid("user", "admin", "super-admin").required().messages({
+    "any.only": "route parameter must be one of [user, admin, super-admin]",
+  }),
+});
