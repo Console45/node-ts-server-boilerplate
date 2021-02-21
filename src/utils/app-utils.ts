2ff1b42672a2ce141c80d4d1b3f848fd5aefc155
Cosmos Appiah <cosmosappiah029@gmail.com>
2021-02-21 11:18:21 +0000

    update

--- src/utils/app-utils.ts
+++ src/utils/app-utils.ts
@@ -2,15 +2,6 @@ import { Response, Request } from "express";
 import compression from "compression";
 
 /**
- *
- * @param {Response} res expess response
- * @param {string} token refresh token
- */
-export const sendRefreshToken = (res: Response, token: string): void => {
-  res.cookie("jid", token, { httpOnly: true, path: "/auth/refresh_token" });
-};
-
-/**
  * Should the compressor compress the respond?
  * @param {Request} req express request object
  * @param {Response} res express response object
