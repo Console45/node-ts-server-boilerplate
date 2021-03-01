16f1460c92c4e1111438d2f1c7068a3d100ef80a
Cosmos Appiah <cosmosappiah029@gmail.com>
2021-03-01 15:07:19 +0000

    added auth middleware

---
+++ src/middlewares/auth.ts
@@ -0,0 +1,19 @@
+import { authServiceInstance } from "./../services/auth";
+import { Response, NextFunction } from "express";
+import { UnAuthorizedRequest } from "../utils/api-error";
+
+export const auth = async (
+  req: any,
+  _: Response,
+  next: NextFunction
+): Promise<void> => {
+  try {
+    const token: string = req.header("Authorization")!.replace("Bearer ", "");
+    const user = authServiceInstance.checkAuth(token);
+    req.user = user;
+    req.accessToken = token;
+    next();
+  } catch (err) {
+    next(new UnAuthorizedRequest("Not authenticated"));
+  }
+};
