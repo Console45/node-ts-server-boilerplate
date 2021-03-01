e3960cad9bb38a1425a6c8f2632114a344ee326c
Cosmos Appiah <cosmosappiah029@gmail.com>
2021-03-01 05:09:04 +0000

    added refresh token even

--- src/utils/event-emitter.ts
+++ src/utils/event-emitter.ts
@@ -7,6 +7,7 @@ import { EventEmitter } from "events";
 export enum Events {
   REGISTER_USER = "register-user",
   LOGIN_USER = "login-user",
+  REFRESH_TOKEN = "refresh-token",
 }
 
 export const eventEmitter = new EventEmitter();
