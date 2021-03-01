13c1e1390f3f60affd500393ce34d348a63df188
Cosmos Appiah <cosmosappiah029@gmail.com>
2021-03-01 19:06:07 +0000

    update

--- src/utils/event-emitter.ts
+++ src/utils/event-emitter.ts
@@ -1,3 +1,7 @@
 import { EventEmitter } from "events";
+import Container, { Token } from "typedi";
 
 export const eventEmitter = new EventEmitter();
+
+export const EVENT_EMITTER_TOKEN = new Token<EventEmitter>("event.emitter");
+Container.set(EVENT_EMITTER_TOKEN, eventEmitter);
